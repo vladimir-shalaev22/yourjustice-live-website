389450382971cdfca6baadb694a810c5ebece57e
Vadim Alexeev <html5css3.pro@gmail.com>
2022-01-13 17:45:49 +0200

    Whitepaper text updates

--- components/WhitepaperChapter/data.js
+++ components/WhitepaperChapter/data.js
@@ -4,50 +4,148 @@ export const links = [
     link: 'an-abstract'
   },
   {
-    text: 'Chapter 1: Problem and Conceptual Solution ',
+    text: {
+      'Chapter 1: Problem and Conceptual Solution ',
+      'Глава 1. Проблема и концептуальное решение'
+    },
     link: 'chapter-one#problem-and-conceptual-solution',
     inners: [
       {
-        text: '3 levels of failures',
+        text: {
+          '3 levels of failures',
+          'Три уровня сбоев'
+        },
         link: 'chapter-one#three-levels-of-failures'
       },
       {
-        text: 'The problem as it is',
+        text: {
+          'The problem as it is',
+          'Суть проблемы'
+        },
         link: 'chapter-one#the-problem-as-it-is'
       },
       {
-        text: 'Why did it happen?',
+        text: {
+          'Why did it happen?',
+          'Почему так произошло?'
+        }, 
         link: 'chapter-one#why-did-it-happen'
       },
       {
-        text: 'Demand for decentralization of relations',
+        text: {
+          'Demand for decentralization of relations',
+          'Спрос на децентрализацию отношений'
+        }, 
         link: 'chapter-one#demand-for-decentralization-of-relations'
       },
       {
-        text: 'The YourJustice Solution',
+        text: {
+          'The YourJustice Solution',
+          'Решение YourJustice'
+        }, 
         link: 'chapter-one#the-yourjustice-solution'
       },
       {
-        text: 'A quick overview of the platform',
+        text: {
+          'A quick overview of the platform',
+          'Краткий обзор платформы'
+        }, 
         link: 'chapter-one#a-quick-overview-of-the-platform'
+      },
+      {
+        text: {
+          'Agents of changes (changemakers), that is, you!',
+          'Агенты изменений. То есть вы!'
+        }, 
+        link: 'agents-of-changes-that-is-yo'
+      },
+      {
+        text: {
+          'What we expect and what we strive for?',
+          'Что мы ожидаем и к чему стремимся?'
+        }, 
+        link: 'what-we-expect-and-what-we-strive-for'
       }
     ]
   },
   {
-    text: 'Chapter 2 - How the Platform Works',
+    text: {
+      'Chapter 2 - How the Platform Works',
+      'Глава 2. Как устроена платформа'
+    },
     link: 'chapter-two#how-the-platform-works',
     inners: [
       {
-        text: 'Demand for decentralization of relations',
-        link: 'chapter-two#demand-for-decentralization-of-relations'
+        text: {
+          'Tiles - a framework for describing role systems',
+          'Тайлы — фреймворк для описания ролевых систем'
+        }, 
+        link: 'tiles'
       },
       {
-        text: 'The YourJustice Solution',
-        link: 'chapter-two#the-yourjustice-solution'
+        text: {
+          'Sample Tiles: Humanity',
+          'Примеры тайлов: Humanity'
+        }, 
+        link: 'humanity'
       },
       {
-        text: 'A quick overview of the platform',
-        link: 'chapter-two#a-quick-overview-of-the-platform'
+        text: {
+          'Sample Tiles: The Tribe of Silence Lovers',
+          'Примеры тайлов: Племя любителей тишины'
+        }, 
+        link: 'silence-lovers'
+      },
+      {
+        text: {
+          'Sample Tiles: Contract',
+          'Примеры тайлов: Контракт'
+        }, 
+        link: 'sample-tiles'
+      },
+      ,
+      {
+        text: {
+          'Tile samples: YJML standardized smart law',
+          'Примеры тайлов: YJML-смарт-закон'
+        }, 
+        link: 'standardized-smart-law'
+      },
+      {
+        text: {
+          'Scores',
+          'Рейтинги'
+        }, 
+        link: 'scores'
+      },
+      {
+        text: {
+          'Courts',
+          'Суды'
+        }, 
+        link: 'courts'
+      },
+      {
+        text: {
+          'Human Entity',
+          'Human Entity'
+        }, 
+        link: 'human-entity'
+      },
+      ,
+      {
+        text: {
+          'Nicknames',
+          'Псевдонимы'
+        }, 
+        link: 'nicknames'
+      },
+      {
+        text: {
+          'Tribal Economics. Cross-Chain Finance',
+          'Экономика племен. Cross-Chain'
+        }, 
+        link: 'cross-chain-finance'
       }
     ]
   }
